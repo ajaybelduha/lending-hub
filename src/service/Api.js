96262dc9ba2dac12e9be73ebb55c63d81ca1109d
Pravesh Choudhary <choudharypravesh03@gmail.com>
2021-01-26 23:06:26 +0530

    followup boss API

---
+++ src/service/Api.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+const token = Buffer.from(`105ff96b20d21612542057ad11a1cfcd789bae`, 'utf8').toString('base64')
+
+export default axios.create({
+  headers: {
+    "X-System":"LendingHubTest",
+    "X-System-Key":"b7580afbf767e392d752053f98f65683",
+    'Authorization': `Basic ${token}`
+  }
+});
