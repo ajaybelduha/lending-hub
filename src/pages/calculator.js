875089b2494248cf1b1d0d4fb745769d6dc39a68
Ajay Kumar <63788715+ajaybelduha@users.noreply.github.com>
2021-11-26 14:06:38 +0530

    Calculator page direct load to show message, calculator is not selected.

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -13,34 +13,38 @@ const Calculator = (props) => {
   }
 
   const setCalculatorType = (id) => {
-    switch (id) {
-      case 'mortgage-payment':
-        setCalculatorHtml(`<div>
-                <h2 class="title-24 mb-6"> Mortgage payment calculator</h2>
-                <div class="widget" data-widget="calc-payment" data-lang="en"></div>
-             </div>`)
-        break
-      case 'affordability':
-        setCalculatorHtml(`<div>
-                <h2 class="title-24 mb-6">Mortgage Affordability Calculator</h2>
-                <div class="widget" data-widget="calc-affordability" data-lang="en"></div>
-                </div>`)
-        break
-      case 'land-transfer-tax':
-        setCalculatorHtml(`<div>
-                <h2 class="title-24 mb-6">Land Transfer Tax Calculator</h2>
-                <div class="widget" data-widget="calc-payment" data-ltt="only" data-lang="en"></div>
-                </div>`)
-        break
-      case 'cmhc-insurance':
-        setCalculatorHtml(`<div>
-                <h2 class="title-24 mb-6">CMHC insurance calculator</h2>
-                <div class="widget" data-widget="calc-payment" data-cmhc="only" data-lang="en"></div>
-                </div>`)
-        break
-      default:
-        setCalculatorHtml('<h1>Please select a proper calculator first</h1>')
-        break
+    if(id !== '' && id !== null && id !== undefined){
+      switch (id) {
+        case 'mortgage-payment':
+          setCalculatorHtml(`<div>
+                  <h2 class="title-24 mb-6"> Mortgage payment calculator</h2>
+                  <div class="widget" data-widget="calc-payment" data-lang="en"></div>
+               </div>`)
+          break
+        case 'affordability':
+          setCalculatorHtml(`<div>
+                  <h2 class="title-24 mb-6">Mortgage Affordability Calculator</h2>
+                  <div class="widget" data-widget="calc-affordability" data-lang="en"></div>
+                  </div>`)
+          break
+        case 'land-transfer-tax':
+          setCalculatorHtml(`<div>
+                  <h2 class="title-24 mb-6">Land Transfer Tax Calculator</h2>
+                  <div class="widget" data-widget="calc-payment" data-ltt="only" data-lang="en"></div>
+                  </div>`)
+          break
+        case 'cmhc-insurance':
+          setCalculatorHtml(`<div>
+                  <h2 class="title-24 mb-6">CMHC insurance calculator</h2>
+                  <div class="widget" data-widget="calc-payment" data-cmhc="only" data-lang="en"></div>
+                  </div>`)
+          break
+        default:
+          setCalculatorHtml('<h1>Please select a proper calculator first.</h1>')
+          break
+      }
+    }else{
+      setCalculatorHtml('<h1>Please select a calculator type first.</h1>')
     }
   }
 
